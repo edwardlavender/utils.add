0b5844093a182b38a12bc0a72787add2284882d9
Edward Lavender <el72@st-andrews.ac.uk>
2020-03-16 17:06:20 +0000

    Add utils.add.R file (i.e. help file for the package).

---
+++ R/utils.add.R
@@ -0,0 +1,8 @@
+#' @title Additional Utilities for Base R
+#' @author Edward Lavender
+#'
+#' @description utils.add provides functions for an eclectic and evolving collection of R operations implemented by the author in the course of scientific research. This includes functions which facilitate data extraction, learning from data, data modification and common mathematical functions.
+#'
+#' @docType package
+#' @name utils.add
+NULL
