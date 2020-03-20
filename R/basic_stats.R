d27e7340863631844a0b4ded08b1330195711e91
Edward Lavender <el72@st-andrews.ac.uk>
2020-03-20 11:42:49 +0000

    Add ... argument to basic_stats() so that arguments that apply to all functions can be passed outside of the list, f.

--- R/basic_stats.R
+++ R/basic_stats.R
@@ -5,6 +5,7 @@
 #' @param f A named list of functions which are used to calculate statistics.
 #' @param p A function for processing summary statistics. The default is a function which rounds each output to the nearest two decimal places.
 #' @param output A character specifying the format in which summary statistics should be returned. The currently implemented options are: (1) \code{"vec"} a numeric vector; (2) \code{"ldf"} a long-format dataframe in long format; and (3) \code{"wdf"}, a wide-format dataframe.
+#' @param ... Other arguments that apply to all functions in \code{f}, such as \code{na.rm = TRUE}.
 #'
 #' @return Basic statistics for a vector of numbers, either in the format of a numeric vector, a long-format dataframe or a wide-format dataframe depending on the input to \code{output} (see above).
 #'
@@ -29,6 +30,9 @@
 #'             output = "ldf"
 #'             )
 #'
+#' #### Example (5) Save typing by passing any arguments that apply to all functions via ...
+#' basic_stats(c(x, NA), f = list(mean = mean, min = min), na.rm = TRUE)
+#'
 #' @author Edward Lavender
 #' @export
 #'
@@ -41,9 +45,9 @@ basic_stats <-
   function(x,
            f = list(mean = mean, min = min, max = max, sd = stats::sd, IQR = stats::IQR),
            p = function(x){ round(x, digits = 2) },
-           output = "ldf"){
+           output = "ldf",...){
   # Apply each function to create a numeric vector with each summary statistic
-  vec <- sapply(f, function(foo){ foo (x) })
+  vec <- sapply(f, function(foo){ foo (x,...) })
   # Apply processing function
   vec <- p(vec)
   # long-format dataframe
