cced89b4284acdc4e471533622a815aa9fc4747a
edwardlavender <edward.lavender@eawag.ch>
2024-12-13 09:51:39 +0100

    Move prettyGraphics to suggests

--- R/utils.add-package.R
+++ R/utils.add-package.R
@@ -3,6 +3,5 @@
 #'
 #' @description utils.add provides functions for an eclectic and evolving collection of R operations implemented by the author in the course of scientific research. This includes functions which facilitate data extraction, learning from data, data modification, common mathematical functions and development tools.
 #'
-#' @docType package
 #' @name utils.add
-NULL
+"_PACKAGE"
